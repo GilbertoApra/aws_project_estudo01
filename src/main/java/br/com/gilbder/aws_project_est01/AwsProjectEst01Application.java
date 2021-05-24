4de1deb66112840e3dbb9920551475744aea9cf3
GilbertoApra <gilberto_dilbert@hotmail.com>
2021-05-23 23:06:12 -0300

    Project Test

---
+++ src/main/java/br/com/gilbder/aws_project_est01/AwsProjectEst01Application.java
@@ -0,0 +1,13 @@
+package br.com.gilbder.aws_project_est01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsProjectEst01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsProjectEst01Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gilbder/aws_project_est01/AwsProjectEst01ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gilbder.aws_project_est01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsProjectEst01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
